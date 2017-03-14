c6ea6361f7c24c691274b96b2840f55fd32cc9d8
liuhui19991 <396604715@qq.com>
2017-03-14 09:19:23 +0800

    更改主题,引入侧边栏

--- app/src/main/java/com/liuhui/world/base/BaseBackActivity.java
+++ app/src/main/java/com/liuhui/world/base/BaseBackActivity.java
@@ -1,20 +1,17 @@
 package com.liuhui.world.base;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-import com.world.studymobilephone.swipeback.SwipeBackActivity;
-import com.world.studymobilephone.utils.Url;
-import com.world.studymobilephone.widget.StatusBarCompat;
+import com.liuhui.world.swipeback.SwipeBackActivity;
+import com.liuhui.world.utils.StatusBarCompat;
 
 import butterknife.ButterKnife;
 
-import static com.world.R.id.toolbar;
 
 /**
  * Created by liuhui on 2016/11/22.
@@ -55,7 +52,7 @@ public abstract class BaseBackActivity<V, P extends BasePresenter<V>> extends Sw
      * @param title 传null可以显示应用名,传""就什么都不显示
      * @param back  true则显示返回键
      */
-    protected void initToolBar(String title, boolean back) {
+    protected void initToolBar(String title, boolean back,int toolbar) {
         mToolbar = findView(toolbar);
         mToolbar.setTitle(title);
         setSupportActionBar(mToolbar);
@@ -68,12 +65,6 @@ public abstract class BaseBackActivity<V, P extends BasePresenter<V>> extends Sw
         return (T) findViewById(id);
     }
 
-    public void startactivity(Activity activity, Class clazz, String url) {
-        Intent intent = new Intent(activity, clazz);
-        intent.putExtra(Url.TYPE, url);
-        startActivity(intent);
-    }
-
     /**
      * ToolBar中的返回按钮对应事件
      */
@@ -84,11 +75,5 @@ public abstract class BaseBackActivity<V, P extends BasePresenter<V>> extends Sw
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    protected void startActivity(String id, Class clazz) {
-        Intent intent = new Intent(mContext, clazz);
-        intent.putExtra(Url.ID, id);
-        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/liuhui/world/ui/fragment/LeftFragment.java
@@ -0,0 +1,28 @@
+package com.liuhui.world.ui.fragment;
+
+
+import com.liuhui.world.R;
+import com.liuhui.world.base.BaseFragment;
+import com.liuhui.world.base.BasePresenter;
+
+/**
+ * Created by liuhui on 2016/11/14.
+ */
+
+public class LeftFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_left;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    public BasePresenter initPresenter() {
+        return null;
+    }
+}
